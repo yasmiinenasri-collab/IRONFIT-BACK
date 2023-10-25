ae9ef65597f4f748471665827c1c88d71238429c
aymenaskri <115466914+aymenaskri@users.noreply.github.com>
2023-10-25 23:16:11 +0100

    Add files via upload

--- First try/src/util/DataSource.java
+++ First try/src/util/DataSource.java
@@ -1,47 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
 /**
  *
- * @author tlili
+ * @author AYMEN ASKRI
  */
 public class DataSource {
-    private Connection cnx;
+     private Connection cnx;
     private static DataSource instance;
     
-    final String URL = "jdbc:mysql://localhost:3306/yassine";
-    final String USR = "root";
-    final String PWD = "";
+    private String url = "jdbc:mysql://localhost:3306/pidev4secrud";
+    private String user = "root";
+    private String password = "";
     
-   
-    
-    private DataSource() {
+    private DataSource(){
         try {
-            cnx = DriverManager.getConnection(URL, USR, PWD);
-            System.out.println("Relation etablie avec succes!");
+            cnx = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to DB !");
         } catch (SQLException ex) {
-             System.out.println(ex.getMessage());
+            System.out.println(ex.getMessage());
         }
-        
     }
-    public static DataSource getInstance() {
-        if(instance == null)
+    
+    public static DataSource getInstance(){
+        if(instance == null){
             instance = new DataSource();
+        }
         return instance;
     }
-
+    
     public Connection getConnection(){
         return this.cnx;
     }
-
 }
-
